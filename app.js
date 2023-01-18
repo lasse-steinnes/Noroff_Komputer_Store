31804193c78ea1307f0dc27b42c2ea2320570815
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-01-18 14:52:20 +0100

    feat: finish functionality
    1) Fix image css inline
    2) finish app.js
    3) balance html fix

--- app.js
+++ app.js
@@ -15,14 +15,15 @@ const btnBuyElem = document.getElementById("btn-buy");
 const descLapTitleElem= document.getElementById("hd-my-comp-title");
 const descLapDescElem= document.getElementById("desc-comp-dd");
 const descLapFeatureElem = document.getElementById("desc-comp-features");
-const imageLapElem = document.getElementById("hd-my-comp-image");
+const imageLapElem = document.getElementById("computerImage");
 
 // Needed parameters: bankValue; loanValue, payValue, tenPercentDeduction;
-let payValue = 0; let tenPercentDeduction = 0; let bankValue = 0; let loanValue = 0;
+let payValue = 0; let tenPercentDeduction = 0; let bankValue = 0; let loanValue = 0; percentage = 0.10;
 
 //              ---------------------------------- BANK ------------------------------------
 // Functions
-function updateBalance(bankValue){
+function updateBalance(){
+    console.log("Balance to HTML - balance", bankValue)
     descbalanceElem.innerHTML = `Balance &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(bankValue)}`;
 }
 
@@ -50,9 +51,11 @@ function getLoan(){
         alert(`You need money in the bank to get a loan`)
     } else { // Get a loan (Max 2x BankValue)
         const prompt = Number(window.prompt(`Enter loan amount in NOK: `));
-        if (loanValue> (2*bankvalue)){
+        console.log("bankvalue ", bankValue);
+        if (prompt > (2*bankValue)){
             alert(`You cannot loan more than 2x your current balance: ${bankValue} NOK`)
         } else { // Get loan, show loan value and activate loan button
+            loanValue = prompt;
             descloanElem.innerHTML = `Loan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(loanValue)}`;
             descloanElem.style.display = "block"  // show loan value in Bank
             btnrepayloanElem.style.display = 'block'; // show optional repay button in Work
@@ -62,8 +65,8 @@ function getLoan(){
 
 //              ---------------------------------- WORK ------------------------------------
 // Functions 
-function updatePay(payValue){
-    descbalanceElem.innerHTML = `Pay &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+function updatePay(){
+    descPayElem.innerHTML = `Pay &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
     ${new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(payValue)}`;
 } 
 
@@ -73,38 +76,44 @@ function getPaid(){ // increase by 100 NOK per work (ie. click)
     ${new Intl.NumberFormat('no-NO', { style: 'currency', currency: 'NOK' }).format(payValue)}`;
 }
 
-function transferPaytoBank(percentage){ // Transfer money to bank, and reset pay, bankvalue and loanValue and Repay button
+function transferPaytoBank(){ // Transfer money to bank, and reset pay, bankvalue and loanValue and Repay button
     console.log("Transfer pay to bank")
     if (loanValue > 0){
         const deduction =  percentage*payValue;
         // add to bank and reduce loan
-        bankValue += payValue - deduction;
+        console.log("percentage", deduction);
+        bankValue = bankValue + payValue - deduction;
         loanValue -= deduction;
         if (loanValue < 0){ // Assuring no negative loan values after payback, give the rest to bank balance
-            bankValue -= loanValue; 
+            console.log("percentage - neg. loan, give to balance")
+            bankValue = bankValue - loanValue; 
+            loanValue = 0;
         }
         payValue = 0;
-        updateBalance(bankValue); updateLoan(); checkLoan(); updatePay(payValue);
+        updateBalance(); updateLoan(); checkLoan(); updatePay();
     } else {
-        payValue = 0;
         bankValue += payValue;
-        updatePay(payValue); updateBalance(bankValue);
+        console.log("get pay")
+        payValue = 0;
+        updatePay(); updateBalance();
     }
 }
 
 function transferPaytoLoan(){ // Transfer all paid money to repay loany, and reset pay
-    console.log("Transfer pay to loan")
+    console.log("Transfer pay to loan");
     if (loanValue < payValue){ // some MAY be transferred to bank
-        loanValue = 0; 
         payValue -= loanValue;
+        loanValue = 0; 
         const promptInput = String(window.prompt(`Transfer surplus to bank (Y/N)?  `));
         if (promptInput == "Y"){
-            bankValue += payValue;
-            updateBalance();
-        } else if (promptInput != "N" && promptInput != "Y"){
-            alert(`Wrong input. Options Y (Yes) or  N (No)`)
+            bankValue += payValue; payValue = 0;
+            //updateBalance(); updatePay(); updateLoan(); checkLoan();
+        } else if (promptInput == "N"){
+            //updateBalance(); updatePay(); 
+        } else if (promptInput != "N" && promptInput != "Y") {
+            alert(`Wrong input. Options Y (Yes) or  N (No)`);
         }
-        updatePay(); checkLoan();
+        updateBalance(); updatePay(); updateLoan(); checkLoan();
     } else { // all will go to repay loan
         loanValue -= payValue; payValue = 0;
         updatePay(); updateLoan(); checkLoan();
@@ -113,11 +122,10 @@ function transferPaytoLoan(){ // Transfer all paid money to repay loany, and res
 
 //              ---------------------------------- LAPTOP ------------------------------------
 // buy laptops
-/*
 function buyLap(){
     if (bankValue >= selectedLap.price) {
         alert('Congratulations! You are the owner of a laptop!');
-        bankValue -= selectedComputer.price;
+        bankValue -= selectedLap.price;
         updateBalance();
     } else {
         alert("I'm sorry to inform you that your current balance is lower than the price. \n\ Please come back when you have more money ");
@@ -126,7 +134,7 @@ function buyLap(){
 
 // load data into an object 
 let laptops = [];
-const pageData = "https://hickory-quilled-actress.glitch.me/computers";
+const pageData = "https://hickory-quilled-actress.glitch.me/computers/";
 fetch(pageData)
     .then(response => response.json()) // With promise
     .then(data => laptops = data) // 
@@ -140,7 +148,14 @@ function addLaptopsToSelect(laptopsObject){
     descLapFeatureElem.innerText =  laptopsObject[0].specs;
     descLapTitleElem.innerText = laptopsObject[0].title;
     descLapDescElem.innerText = laptopsObject[0].description;
-    imageLapElem.src = pageData +  laptopsObject[0].image;
+    console.log("image-elem ", laptopsObject[0].image)
+    let text =  "https://hickory-quilled-actress.glitch.me/" + laptopsObject[0].image;
+    if (laptopsObject[0].id != 5){
+        imageLapElem.src = text;
+    } else {
+        imageLapElem.src = text.replace("jpg","png");
+    }
+    //imageLapElem.src =  "https://hickory-quilled-actress.glitch.me/" + laptopsObject[0].image; //pageData +  String(laptopsObject[0].image);
 }
 
 // function to select a laptop
@@ -156,14 +171,18 @@ function addOneToSelect(laptop) {
     descPriceElem.innerText = new Intl.NumberFormat('no-NO', {style: 'currency', currency: 'NOK'}).format(selectedLap.price);
     descLapFeatureElem.innerText = selectedLap.specs;
     descLapTitleElem.innerText = selectedLap.title;
-    descLapDescElem.innerText = selectedLap.description;
-    imageLapElem.src = "https://hickory-quilled-actress.glitch.me/computers" + selectedLap.image;
+    descLapDescElem.innerTex    t = selectedLap.description;
+    let text = `https://hickory-quilled-actress.glitch.me/${selectedLap.image}`;
+    if (selectedLap.id != 5){
+        imageLapElem.src = text;
+    } else {
+        imageLapElem.src = text.replace("jpg","png");
+    }
 } 
-*/
 //              ---------------------------------- ClICKABLE EVENTS :D ------------------------------------
 btntransferBankElem.addEventListener("click", transferPaytoBank); // to the right <-- function
 btnrepayloanElem.addEventListener('click', transferPaytoLoan);
 btnWorkElem.addEventListener('click', getPaid);
 btngetloanElem.addEventListener('click', getLoan);
-//btnBuyElem.addEventListener('click',buyLap)
-//selectLaptopElem.addEventListener("change", handleLapChange);
+btnBuyElem.addEventListener('click',buyLap)
+selectLaptopElem.addEventListener("change", handleLapChange);
